8f4e522ce3a10c4681177c126ed0d03deab213df
Renato Gonzales <renato.gonzales@bizlinks.la>
2021-03-15 20:16:46 -0500

    Primer codigo JS

---
+++ semana4/semana17/variables/codigo.js
@@ -0,0 +1,19 @@
+"use strict;"
+let nombre = "Renato";
+let sistema = "WSL";
+console.log(typeof nombre);
+
+console.log(
+    `Mi segundo nombre es  ${nombre} y mi sistema operativo es ${sistema}`
+    
+)
+
+let miEdad = 24;
+let miPeso = 80.500;
+
+let altura = 1.75;
+let peso = 90.500
+
+//bmi = indice de masa corporal
+let bmi = (peso) / (altura ** 2)
+document.write(bmi)
