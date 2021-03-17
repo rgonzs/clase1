09d4fa00669ed629eec0d6350157c6d6848a8fcd
Renato Gonzales <renato.gonzales@bizlinks.la>
2021-03-16 20:42:32 -0500

    Añdadida la funcion printName

--- semana4/clase18/index.js
+++ semana4/clase18/index.js
@@ -1,10 +1,30 @@
-cancion = "cancion1"
+cancion = "cancion1";
 if (cancion == "cancion1") {
-    console.log("Cancion 1 letra")
-}else if(cancion == "cancion2") {
-    console.log("Cancion 2 letra")
-}else if(cancion == "cancion3") {
-    console.log("Cancion 3 letra")
-}else{
-    console.log("no hay pe")
+  console.log("Cancion 1 letra");
+} else if (cancion == "cancion2") {
+  console.log("Cancion 2 letra");
+} else if (cancion == "cancion3") {
+  console.log("Cancion 3 letra");
+} else {
+  console.log("no hay pe");
 }
+
+function miFuncion() {
+  console.log("Llamada de Funcion 1");
+  console.log("Llamada de Funcion 2");
+  console.log("Llamada de Funcion 3");
+}
+
+function printName(name) {
+  let miNombre;
+  if (name === "" || name === undefined || name === "undefined" || name === "None") {
+    miNombre = "Debes ingresar un nombre valido";
+  } else {
+    miNombre = `Mi nombre es ${name}.`;
+  }
+  return miNombre;
+}
+
+console.log(printName("Renato"));
+console.log(printName("None"));
+console.log(printName());
