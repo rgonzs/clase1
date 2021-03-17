e95fc448e06b0266087b44c8b980913eac681398
Renato Gonzales <renato.gonzales@bizlinks.la>
2021-03-16 19:41:23 -0500

    Se añadio el ejercicio con if

---
+++ semana4/clase18/index.js
@@ -0,0 +1,10 @@
+cancion = "cancion1"
+if (cancion == "cancion1") {
+    console.log("Cancion 1 letra")
+}else if(cancion == "cancion2") {
+    console.log("Cancion 2 letra")
+}else if(cancion == "cancion3") {
+    console.log("Cancion 3 letra")
+}else{
+    console.log("no hay pe")
+}
