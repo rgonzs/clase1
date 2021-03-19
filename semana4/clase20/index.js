346f63262e3cd307ebb6a9e2b78c86af4b6bf76f
Renato Gonzales <renato.gonzales@bizlinks.la>
2021-03-18 20:27:20 -0500

    Trabajando con bucle while

--- semana4/clase20/index.js
+++ semana4/clase20/index.js
@@ -9,13 +9,23 @@
 //     console.log(numero)
 //   }
 // }
-let suma = 0
+let suma = 0;
 for (let numero = 0; numero < 1000; numero++) {
-  
+
   if (numero % 3 === 0 || numero % 5 === 0) {
     suma = suma + numero
     console.log(numero)
   }
-  
+
 }
-console.log(suma)
+let suma2 = 0;
+let numero2 = 0;
+
+while (numero2 < 1000) {
+  if (numero2 % 3 === 0 || numero2 % 5 === 0) {
+    suma2 = suma2 + numero2;
+    console.log(numero2);
+  }
+  numero2++
+}
+console.log(suma2);
